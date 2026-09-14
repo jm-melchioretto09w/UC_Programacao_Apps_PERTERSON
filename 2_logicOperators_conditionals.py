17dede29f3d1229efbc3006a95fe33ae5f78b394
JOÃO MARCOS MELCHIORETTO <joao_melchioretto150@estudante.sesisenai.org.br>
2026-09-14 08:31:45 -0300

    aula 2

---
+++ 2_logicOperators_conditionals.py
@@ -0,0 +1,14 @@
+num0 = int(input("Primeiro número: "))
+num1 = int(input("Segundo número: "))
+
+print(num0 == num1)
+print(num0 != num1)
+print(num0 < num1)
+print(num0 > num1)
+print(num0 <= num1)
+print(num0 >= num1)
+
+if num0 == num1:
+    print("os números são iguais")
+else:
+    print("os números não são iguais")
